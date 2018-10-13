f45b9da5f98dc33b36cc910bf19a5a7a46fc3b9a
Stefano Balzarotti <stefano.balzarotti@orbintsoft.net>
2018-10-13 18:52:00 +0200

    Add tests ti nornalize

--- OrbintSoft.Yauaa.NetStandard/OrbintSoft.Yauaa.NetStandard.Analyzer.Test/Parse/UserAgentNS/Utils/TestNormalize.cs
+++ OrbintSoft.Yauaa.NetStandard/OrbintSoft.Yauaa.NetStandard.Analyzer.Test/Parse/UserAgentNS/Utils/TestNormalize.cs
@@ -67,9 +67,33 @@ namespace OrbintSoft.Yauaa.Analyzer.Test.Parse.UserAgentNS.Utils
             Normalize.CleanupDeviceBrandName("AsUs", "something t123").Should().Be("Asus Something T123");
             Normalize.CleanupDeviceBrandName("Sony", "sony x1").Should().Be("Sony X1");
             Normalize.CleanupDeviceBrandName("Sony", "sony-x1").Should().Be("Sony X1");
-            //Normalize.CleanupDeviceBrandName("Sony", "sonyx1").Should().Be("Sony X1");
-            //Normalize.CleanupDeviceBrandName("hP", "SlateBook 10 X2 PC").Should().Be("HP SlateBook 10 X2 PC");
-            //Normalize.CleanupDeviceBrandName("Samsung", "GT - 1234").Should().Be("Samsung GT-1234");
+            Normalize.CleanupDeviceBrandName("Sony", "sonyx1").Should().Be("Sony X1");
+            Normalize.CleanupDeviceBrandName("hP", "SlateBook 10 X2 PC").Should().Be("HP SlateBook 10 X2 PC");
+            Normalize.CleanupDeviceBrandName("Samsung", "GT - 1234").Should().Be("Samsung GT-1234");
+        }
+
+        [Fact]
+        public void CheckEmailNormalization()
+        {
+            Normalize.Email("support [at] zite [dot] com").Should().Be("support@zite.com");
+            Normalize.Email("austin at affectv dot co dot uk").Should().Be("austin@affectv.co.uk");
+            Normalize.Email("epicurus at gmail dot com").Should().Be("epicurus@gmail.com");
+            Normalize.Email("buibui[dot]bot[\\xc3\\xa07]moquadv[dot]com").Should().Be("buibui.bot@moquadv.com");
+            Normalize.Email("maxpoint.crawler at maxpointinteractive dot com").Should().Be("maxpoint.crawler@maxpointinteractive.com");
+            Normalize.Email("help@moz.com").Should().Be("help@moz.com");
+            Normalize.Email("crawler at example dot com").Should().Be("crawler@example.com");
+            Normalize.Email("yelpbot at yelp dot com").Should().Be("yelpbot@yelp.com");
+            Normalize.Email("support [at] zite [dot] com").Should().Be("support@zite.com");
+            Normalize.Email("support [at] safedns [dot] com").Should().Be("support@safedns.com");
+            Normalize.Email("search_comments\\at\\sensis\\dot\\com\\dot\\au").Should().Be("search_comments@sensis.com.au");
+            Normalize.Email("mms dash mmaudvidcrawler dash support at yahoo dash inc dot com").Should().Be("mms-mmaudvidcrawler-support@yahoo-inc.com");
+        }
+
+        [Fact]
+        public void CheckBadInputData()
+        {
+            // This used to trigger an exception in the underlying RegEx.
+            Normalize.CleanupDeviceBrandName("${N", "${N.Foo");
         }
     }
 }
