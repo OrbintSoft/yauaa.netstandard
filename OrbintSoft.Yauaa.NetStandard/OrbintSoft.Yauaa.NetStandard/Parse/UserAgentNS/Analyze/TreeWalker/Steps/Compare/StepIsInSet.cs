324b14752fdb855718c1e9eaf981e142de78629c
Stefano Balzarotti <stefano.balzarotti@orbintsoft.net>
2018-11-11 14:55:43 +0100

    add https

--- OrbintSoft.Yauaa.NetStandard/OrbintSoft.Yauaa.NetStandard/Parse/UserAgentNS/Analyze/TreeWalker/Steps/Compare/StepIsInSet.cs
+++ OrbintSoft.Yauaa.NetStandard/OrbintSoft.Yauaa.NetStandard/Parse/UserAgentNS/Analyze/TreeWalker/Steps/Compare/StepIsInSet.cs
@@ -11,7 +11,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ * https://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -22,9 +22,9 @@
  * All rights should be reserved to the original author Niels Basjes
  */
 
+using Antlr4.Runtime.Tree;
 using System;
 using System.Collections.Generic;
-using Antlr4.Runtime.Tree;
 
 namespace OrbintSoft.Yauaa.Analyzer.Parse.UserAgentNS.Analyze.TreeWalker.Steps.Compare
 {
