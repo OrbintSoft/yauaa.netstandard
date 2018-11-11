5b38f07f0011210dbbba9c509012624a406f76f0
Stefano Balzarotti <stefano.balzarotti@orbintsoft.net>
2018-11-11 14:51:11 +0100

    add https

--- OrbintSoft.Yauaa.NetStandard/OrbintSoft.Yauaa.NetStandard/Parse/UserAgentNS/Analyze/TreeWalker/Steps/Compare/StepContains.cs
+++ OrbintSoft.Yauaa.NetStandard/OrbintSoft.Yauaa.NetStandard/Parse/UserAgentNS/Analyze/TreeWalker/Steps/Compare/StepContains.cs
@@ -11,7 +11,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ * https://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -30,7 +30,6 @@ namespace OrbintSoft.Yauaa.Analyzer.Parse.UserAgentNS.Analyze.TreeWalker.Steps.C
     [Serializable]
     public class StepContains: Step
     {
-
         private readonly string desiredValue;
 
         public StepContains(string desiredValue)
