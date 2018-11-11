616974bded35c23daef5fa63ac001fcc4a5856ac
Stefano Balzarotti <stefano.balzarotti@orbintsoft.net>
2018-11-11 19:23:26 +0100

    improve error handling

--- OrbintSoft.Yauaa.NetStandard/OrbintSoft.Yauaa.NetStandard/Parse/UserAgentNS/Utils/YamlUtils.cs
+++ OrbintSoft.Yauaa.NetStandard/OrbintSoft.Yauaa.NetStandard/Parse/UserAgentNS/Utils/YamlUtils.cs
@@ -23,10 +23,10 @@
  */
 
 using OrbintSoft.Yauaa.Analyzer.Parse.UserAgentNS.Analyze;
-using System.Collections.Generic;
-using YamlDotNet.RepresentationModel;
-using System.Linq;
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using YamlDotNet.RepresentationModel;
 
 namespace OrbintSoft.Yauaa.Analyzer.Parse.UserAgentNS.Utils
 {
@@ -52,11 +52,6 @@ namespace OrbintSoft.Yauaa.Analyzer.Parse.UserAgentNS.Utils
             }
         }
 
-        public static void Fail(YamlNode node, string filename, string error)
-        {
-            throw new InvalidParserConfigurationException(CreateErrorString(node, filename, error));
-        }
-
         private static string CreateErrorString(YamlNode node, string filename, string error)
         {
             return "Yaml config problem.(" + filename + ":" + node.Start.Line + "): " + error;
@@ -65,62 +60,46 @@ namespace OrbintSoft.Yauaa.Analyzer.Parse.UserAgentNS.Utils
         public static KeyValuePair<YamlNode,YamlNode> GetExactlyOneNodeTuple(YamlMappingNode mappingNode, string filename)
         {
             var nodeTupleList = mappingNode.ToList();
-            if (nodeTupleList.Count != 1)
-            {
-                Fail(mappingNode, filename, "There must be exactly 1 value in the list");
-            }
+            Require(nodeTupleList.Count == 1, mappingNode, filename, "There must be exactly 1 value in the list");
             return nodeTupleList.FirstOrDefault();
         }
 
         public static string GetKeyAsString(KeyValuePair<YamlNode, YamlNode> tuple, string filename)
         {
             YamlNode keyNode = tuple.Key;
-            if (!(keyNode is YamlScalarNode)) {
-                Fail(tuple.Key, filename, "The key should be a string but it is a " + keyNode.NodeType.ToString());
-            }
+            RequireNodeInstanceOf(typeof(YamlScalarNode), tuple.Key, filename, "The key should be a string but it is a " + keyNode.NodeType.ToString());
             return ((YamlScalarNode)keyNode).Value;
         }
 
         public static string GetValueAsString(KeyValuePair<YamlNode, YamlNode> tuple, string filename)
         {
             YamlNode valueNode = tuple.Value;
-            if (!(valueNode is YamlScalarNode)) {
-                Fail(tuple.Value, filename, "The value should be a string but it is a " + valueNode.NodeType.ToString());
-            }
+            RequireNodeInstanceOf(typeof(YamlScalarNode), tuple.Value, filename, "The value should be a string but it is a " + valueNode.NodeType.ToString());
             return ((YamlScalarNode)valueNode).Value;
         }
 
         public static YamlMappingNode GetValueAsMappingNode(KeyValuePair<YamlNode, YamlNode> tuple, string filename)
         {
             YamlNode valueNode = tuple.Value;
-            if (!(valueNode is YamlMappingNode)) {
-                Fail(tuple.Value, filename, "The value should be a map but it is a " + valueNode.NodeType.ToString());
-            }
+            RequireNodeInstanceOf(typeof(YamlMappingNode), tuple.Value, filename, "The value should be a map but it is a " + valueNode.NodeType.ToString());
             return ((YamlMappingNode)valueNode);
         }
 
         public static YamlSequenceNode GetValueAsSequenceNode(KeyValuePair<YamlNode, YamlNode> tuple, string filename)
         {
             YamlNode valueNode = tuple.Value;
-            if (!(valueNode is YamlSequenceNode)) {
-                Fail(tuple.Value, filename, "The value should be a sequence but it is a " + valueNode.NodeType.ToString());
-            }
+            RequireNodeInstanceOf(typeof(YamlSequenceNode), tuple.Value, filename, "The value should be a sequence but it is a " + valueNode.NodeType.ToString());
             return ((YamlSequenceNode)valueNode);
         }
 
         public static List<string> GetStringValues(YamlNode sequenceNode, string filename)
         {
-            if (!(sequenceNode is YamlSequenceNode)) {
-                Fail(sequenceNode, filename, "The provided node must be a sequence but it is a " + sequenceNode.NodeType.ToString());
-            }
-
+            RequireNodeInstanceOf(typeof(YamlSequenceNode), sequenceNode, filename, "The provided node must be a sequence but it is a " + sequenceNode.NodeType.ToString());
             List<YamlNode> valueNodes = ((YamlSequenceNode)sequenceNode).ToList();
             List<string> values = new List<string>();
             foreach (YamlNode node in valueNodes)
             {
-                if (!(node is YamlScalarNode)) {
-                    Fail(node, filename, "The value should be a string but it is a " + node.NodeType.ToString());
-                }
+                RequireNodeInstanceOf(typeof(YamlScalarNode), node, filename, "The value should be a string but it is a " + node.NodeType.ToString());
                 values.Add(((YamlScalarNode)node).Value);
             }
             return values;
