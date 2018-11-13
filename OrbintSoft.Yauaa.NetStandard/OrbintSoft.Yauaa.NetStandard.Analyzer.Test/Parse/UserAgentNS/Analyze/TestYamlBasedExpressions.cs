746c86b95cbc5faed61ab294c7594e0038b1fd6e
Stefano Balzarotti <stefano.balzarotti@orbintsoft.net>
2018-11-13 21:04:33 +0100

    refactoring

--- OrbintSoft.Yauaa.NetStandard/OrbintSoft.Yauaa.NetStandard.Analyzer.Test/Parse/UserAgentNS/Analyze/TestYamlBasedExpressions.cs
+++ OrbintSoft.Yauaa.NetStandard/OrbintSoft.Yauaa.NetStandard.Analyzer.Test/Parse/UserAgentNS/Analyze/TestYamlBasedExpressions.cs
@@ -1,10 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-using Xunit;
+﻿/*
+ * Yet Another UserAgent Analyzer .NET Standard
+ * Porting realized by Balzarotti Stefano, Copyright (C) OrbintSoft
+ * 
+ * Original Author and License:
+ * 
+ * Yet Another UserAgent Analyzer
+ * Copyright (C) 2013-2018 Niels Basjes
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * All rights should be reserved to the original author Niels Basjes
+ */
+
 using FluentAssertions;
 using OrbintSoft.Yauaa.Analyzer.Parse.UserAgentNS.Debug;
 using OrbintSoft.Yauaa.Analyzer.Test.Fixtures;
+using Xunit;
 
 namespace OrbintSoft.Yauaa.Analyzer.Test.Parse.UserAgentNS.Analyze
 {
@@ -76,6 +97,7 @@ namespace OrbintSoft.Yauaa.Analyzer.Test.Parse.UserAgentNS.Analyze
         public void RunWalkingTests()
         {
             UserAgentAnalyzerTester uaa = new UserAgentAnalyzerTester("YamlResources", "Walking-tests.yaml");
+            uaa.SetVerbose(true);
             uaa.RunTests(false, false).Should().BeTrue();
         }
 
@@ -104,6 +126,7 @@ namespace OrbintSoft.Yauaa.Analyzer.Test.Parse.UserAgentNS.Analyze
         public void RunAllPossibleSteps()
         {
             UserAgentAnalyzerTester uaa = new UserAgentAnalyzerTester("YamlResources", "AllPossibleSteps.yaml");
+            uaa.SetVerbose(true);
             uaa.RunTests(false, false).Should().BeTrue();
         }
     }
