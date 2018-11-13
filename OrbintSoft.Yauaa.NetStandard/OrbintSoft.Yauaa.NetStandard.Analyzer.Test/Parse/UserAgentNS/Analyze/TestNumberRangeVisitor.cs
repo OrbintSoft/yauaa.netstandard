1fb7f1031dd60a0fd2817c2a30733715ad0267d4
Stefano Balzarotti <stefano.balzarotti@orbintsoft.net>
2018-11-13 20:44:37 +0100

    Add TestRangeCompare

--- OrbintSoft.Yauaa.NetStandard/OrbintSoft.Yauaa.NetStandard.Analyzer.Test/Parse/UserAgentNS/Analyze/TestNumberRangeVisitor.cs
+++ OrbintSoft.Yauaa.NetStandard/OrbintSoft.Yauaa.NetStandard.Analyzer.Test/Parse/UserAgentNS/Analyze/TestNumberRangeVisitor.cs
@@ -1,10 +1,34 @@
-﻿using OrbintSoft.Yauaa.Analyzer.Parse.UserAgentNS.Analyze;
+﻿/*
+ * Yet Another UserAgent Analyzer .NET Standard
+ * Porting realized by Balzarotti Stefano, Copyright (C) OrbintSoft
+ * 
+ * Original Author and License:
+ * 
+ * Yet Another UserAgent Analyzer
+ * Copyright (C) 2013-2018 Niels Basjes
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * All rights should be reserved to the original author Niels Basjes
+ */
+
+using FluentAssertions;
+using OrbintSoft.Yauaa.Analyzer.Parse.UserAgentNS.Analyze;
+using OrbintSoft.Yauaa.Analyzer.Test.Fixtures;
 using System;
 using System.Collections.Generic;
-using Xunit;
-using FluentAssertions;
 using System.Linq;
-using OrbintSoft.Yauaa.Analyzer.Test.Fixtures;
+using Xunit;
 
 namespace OrbintSoft.Yauaa.Analyzer.Test.Parse.UserAgentNS.Analyze
 {
@@ -44,6 +68,23 @@ namespace OrbintSoft.Yauaa.Analyzer.Test.Parse.UserAgentNS.Analyze
             values.Contains(7).Should().BeFalse();
             values.Contains(8).Should().BeFalse();
             values.Contains(9).Should().BeFalse();
+        }
+
+        [Fact]
+        public void TestRangeCompare()
+        {
+            WordRangeVisitor.Range range1 = new WordRangeVisitor.Range(1, 2);
+            WordRangeVisitor.Range range1b = new WordRangeVisitor.Range(1, 2);
+            WordRangeVisitor.Range range2 = new WordRangeVisitor.Range(2, 1);
+            WordRangeVisitor.Range range3 = new WordRangeVisitor.Range(1, 1);
+            WordRangeVisitor.Range range4 = new WordRangeVisitor.Range(2, 2);
+            string notARange = "Range";
+
+            range1.Should().BeEquivalentTo(range1b);
+            range1.Equals(range2).Should().BeFalse();
+            range1.Equals(range3).Should().BeFalse();
+            range1.Equals(range4).Should().BeFalse();
+            range1.Equals(notARange).Should().BeFalse();
         }
     }
 }
