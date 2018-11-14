798cf2ce16d2819f7454a7ba8de9f6d6b0f0aaa8
Stefano Balzarotti <stefano.balzarotti@orbintsoft.net>
2018-11-14 13:49:31 +0100

    refactoring

--- OrbintSoft.Yauaa.NetStandard/OrbintSoft.Yauaa.NetStandard.Analyzer.Test/Parse/UserAgentNS/Parse/TestUserAgentFlattening.cs
+++ OrbintSoft.Yauaa.NetStandard/OrbintSoft.Yauaa.NetStandard.Analyzer.Test/Parse/UserAgentNS/Parse/TestUserAgentFlattening.cs
@@ -1,12 +1,35 @@
-﻿using log4net;
+﻿/*
+ * Yet Another UserAgent Analyzer .NET Standard
+ * Porting realized by Balzarotti Stefano, Copyright (C) OrbintSoft
+ * 
+ * Original Author and License:
+ * 
+ * Yet Another UserAgent Analyzer
+ * Copyright (C) 2013-2018 Niels Basjes
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * 
+ * All rights should be reserved to the original author Niels Basjes
+ */
+
+using log4net;
 using OrbintSoft.Yauaa.Analyzer.Parse.UserAgentNS;
+using OrbintSoft.Yauaa.Analyzer.Test.Fixtures;
 using System;
 using System.Collections.Generic;
 using System.Text;
-using Xunit;
-using FluentAssertions;
 using System.Text.RegularExpressions;
-using OrbintSoft.Yauaa.Analyzer.Test.Fixtures;
+using Xunit;
 
 namespace OrbintSoft.Yauaa.Analyzer.Test.Parse.UserAgentNS.Parse
 {
@@ -28,9 +51,9 @@ namespace OrbintSoft.Yauaa.Analyzer.Test.Parse.UserAgentNS.Parse
             // Special case with multiple spaces between words
             ValidateUserAgent(
                 "one two  three   four/1"
-                , "agent=\"one two  three   four/1\""
-                , "agent.(1)product=\"one two  three   four/1\""
-                , "agent.(1)product.(1)name=\"one two  three   four\""
+                , "agent=\"one two three four/1\""
+                , "agent.(1)product=\"one two three four/1\""
+                , "agent.(1)product.(1)name=\"one two three four\""
                 , "agent.(1)product.(1)version=\"1\""
             );
 
