f6dc5a366d8fc617202b8baf56d8df1438fe97c4
Stefano Balzarotti <stefano.balzarotti@orbintsoft.net>
2018-11-15 19:09:50 +0100

    insert standard licenseheader to all files, using LicenseHeaderManager

--- OrbintSoft.Yauaa.NetStandard/OrbintSoft.Yauaa.NetStandard.Analyzer.Test/AssemblyInfo.cs
+++ OrbintSoft.Yauaa.NetStandard/OrbintSoft.Yauaa.NetStandard.Analyzer.Test/AssemblyInfo.cs
@@ -1,1 +1,27 @@
-﻿[assembly: log4net.Config.XmlConfigurator(Watch = true, ConfigFile = "log4net.config")]
+﻿//<copyright file="AssemblyInfo.cs" company="OrbintSoft">
+//	Yet Another UserAgent Analyzer.NET Standard
+//	Porting realized by Stefano Balzarotti, Copyright (C) OrbintSoft
+//
+//	Original Author and License:
+//
+//	Yet Another UserAgent Analyzer
+//	Copyright(C) 2013-2018 Niels Basjes
+//
+//	Licensed under the Apache License, Version 2.0 (the "License");
+//	you may not use this file except in compliance with the License.
+//	You may obtain a copy of the License at
+//
+//	https://www.apache.org/licenses/LICENSE-2.0
+//
+//	Unless required by applicable law or agreed to in writing, software
+//	distributed under the License is distributed on an "AS IS" BASIS,
+//	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//	See the License for the specific language governing permissions and
+//	limitations under the License.
+//
+//</copyright>
+//<author>Stefano Balzarotti, Niels Basjes</author>
+//<date>2018, 10, 15, 20:11</date>
+//<summary></summary>
+
+[assembly: log4net.Config.XmlConfigurator(Watch = true, ConfigFile = "log4net.config")]
--- OrbintSoft.Yauaa.NetStandard/OrbintSoft.Yauaa.NetStandard.Analyzer.Test/Parse/UserAgentNS/Profile/TestMemoryFootprint.cs
+++ OrbintSoft.Yauaa.NetStandard/OrbintSoft.Yauaa.NetStandard.Analyzer.Test/Parse/UserAgentNS/Profile/TestMemoryFootprint.cs
@@ -1,26 +1,28 @@
-﻿/*
- * Yet Another UserAgent Analyzer .NET Standard
- * Porting realized by Balzarotti Stefano, Copyright (C) OrbintSoft
- * 
- * Original Author and License:
- * 
- * Yet Another UserAgent Analyzer
- * Copyright (C) 2013-2018 Niels Basjes
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * https://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * 
- * All rights should be reserved to the original author Niels Basjes
- */
+﻿//<copyright file="TestMemoryFootprint.cs" company="OrbintSoft">
+//	Yet Another UserAgent Analyzer.NET Standard
+//	Porting realized by Stefano Balzarotti, Copyright (C) OrbintSoft
+//
+//	Original Author and License:
+//
+//	Yet Another UserAgent Analyzer
+//	Copyright(C) 2013-2018 Niels Basjes
+//
+//	Licensed under the Apache License, Version 2.0 (the "License");
+//	you may not use this file except in compliance with the License.
+//	You may obtain a copy of the License at
+//
+//	https://www.apache.org/licenses/LICENSE-2.0
+//
+//	Unless required by applicable law or agreed to in writing, software
+//	distributed under the License is distributed on an "AS IS" BASIS,
+//	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//	See the License for the specific language governing permissions and
+//	limitations under the License.
+//
+//</copyright>
+//<author>Stefano Balzarotti, Niels Basjes</author>
+//<date>2018, 11, 14, 20:22</date>
+//<summary></summary>
 
 using log4net;
 using OrbintSoft.Yauaa.Analyzer.Parse.UserAgentNS;
