cbc869de27417fe6780437e4a63b83604b48f0b2
Stefano Balzarotti <stefano.balzarotti@orbintsoft.net>
2018-11-24 16:28:16 +0100

    update license

--- OrbintSoft.Yauaa.NetStandard/OrbintSoft.Yauaa.NetStandard/AssemblyInfo.cs
+++ OrbintSoft.Yauaa.NetStandard/OrbintSoft.Yauaa.NetStandard/AssemblyInfo.cs
@@ -1,32 +1,32 @@
-﻿//<copyright file="AssemblyInfo.cs" company="OrbintSoft">
-//	Yet Another UserAgent Analyzer.NET Standard
-//	Porting realized by Stefano Balzarotti, Copyright (C) OrbintSoft
+﻿//-----------------------------------------------------------------------
+// <copyright file="AssemblyInfo.cs" company="OrbintSoft">
+//    Yet Another UserAgent Analyzer.NET Standard
+//    orting realized by Stefano Balzarotti, Copyright (C) OrbintSoft
 //
-//	Original Author and License:
+//    Original Author and License:
 //
-//	Yet Another UserAgent Analyzer
-//	Copyright(C) 2013-2018 Niels Basjes
+//    Yet Another UserAgent Analyzer
+//    Copyright(C) 2013-2018 Niels Basjes
 //
-//	Licensed under the Apache License, Version 2.0 (the "License");
-//	you may not use this file except in compliance with the License.
-//	You may obtain a copy of the License at
+//    Licensed under the Apache License, Version 2.0 (the "License");
+//    you may not use this file except in compliance with the License.
+//    You may obtain a copy of the License at
 //
-//	https://www.apache.org/licenses/LICENSE-2.0
+//    https://www.apache.org/licenses/LICENSE-2.0
 //
-//	Unless required by applicable law or agreed to in writing, software
-//	distributed under the License is distributed on an "AS IS" BASIS,
-//	WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-//	See the License for the specific language governing permissions and
-//	limitations under the License.
-//
-//</copyright>
-//<author>Stefano Balzarotti, Niels Basjes</author>
-//<date>2018, 10, 15, 20:12</date>
-//<summary></summary>
-
+//    Unless required by applicable law or agreed to in writing, software
+//    distributed under the License is distributed on an "AS IS" BASIS,
+//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//    See the License for the specific language governing permissions and
+//    limitations under the License.
+//   
+// </copyright>
+// <author>Stefano Balzarotti, Niels Basjes</author>
+// <date>2018, 11, 19, 16:21</date>
+// <summary></summary>
+//-----------------------------------------------------------------------
 using System;
 using System.Runtime.CompilerServices;
-
 
 [assembly: InternalsVisibleTo("OrbintSoft.Yauaa.NetCore.Analyzer.Test, PublicKey=00240000048000009400000006020000002400005253413100040000010001001da7ed2aac4ff100180d06b0fff5a3c4ebfe9e5a1e6c4550f3e6b0efa73090fb8f5aba93b27c16c420bf7fd598573b7aa97cce3cd6c57756dd8d2676a24cce1c400512de19d38b92e139b2191cbadaae155f1cc4d202d16030a1351d3a9b431a4edc2a4cd3a4cd0646785d078c1150aadda412c00bc9377ec8f76940674a1dca")]
 [assembly: CLSCompliant(false)]
