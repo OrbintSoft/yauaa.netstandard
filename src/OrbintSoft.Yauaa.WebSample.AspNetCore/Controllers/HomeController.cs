6b63952dc4a8dd6ccba4016b8e0b98cce52f0fcb
Stefano Balzarotti <stefano.balzarotti@orbintsoft.net>
2018-12-14 23:28:13 +0100

    update to core 2.2

--- src/OrbintSoft.Yauaa.WebSample.AspNetCore/Controllers/HomeController.cs
+++ src/OrbintSoft.Yauaa.WebSample.AspNetCore/Controllers/HomeController.cs
@@ -33,11 +33,5 @@ namespace OrbintSoft.Yauaa.WebSample.AspNetCore.Controllers
         {
             return View();
         }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
     }
 }
